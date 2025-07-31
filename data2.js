a5204bc83b8a59176d379920cd24bee5291024b6
Thuydothi2004 <thuydothi042412@gmail.com>
2025-07-31 11:46:16 +0700

    Add files via upload

---
+++ data2.js
@@ -0,0 +1,7 @@
+const data = [
+  { id: 1, khachHang: "Nguyễn Văn A", nhanVien: "Trần Thị B", giaoDich: "Mua hàng" },
+  { id: 2, khachHang: "Lê Hoàng", nhanVien: "Phạm Cường", giaoDich: "Thanh toán" },
+  { id: 3, khachHang: "Trần Minh", nhanVien: "Lý Lan", giaoDich: "Hoàn tiền" },
+  { id: 4, khachHang: "Ngô Mai", nhanVien: "Nguyễn Tuấn", giaoDich: "Đặt hàng" },
+  { id: 5, khachHang: "Phan Quốc", nhanVien: "Võ Hằng", giaoDich: "Tư vấn" }
+];
