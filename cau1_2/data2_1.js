ec8974a95260dbd7da5e10daf1f66868c476c85f
Thuydothi2004 <thuydothi042412@gmail.com>
2025-08-03 22:55:47 +0700

    Add files via upload

---
+++ cau1_2/data2_1.js
@@ -0,0 +1,8 @@
+// data.js
+const userData = [
+  { name: "Nguyễn Văn A", phone: "0912345678" },
+  { name: "Trần Thị B", phone: "0987654321" },
+  { name: "Lê Văn C", phone: "0909090909" },
+  { name: "Phạm Thị D", phone: "0933333333" },
+  { name: "Hoàng Văn E", phone: "0977777777" }
+];
---
+++ cau1_2/data2_3.js
@@ -0,0 +1,23 @@
+// data.js
+const peopleData = [
+  {
+    firstName: "An",
+    middleName: "Nguyễn Văn",
+    address: "123 Lê Lợi, Quận 1, TP.HCM"
+  },
+  {
+    firstName: "Bình",
+    middleName: "Trần Thị",
+    address: "45 Nguyễn Huệ, TP Đà Nẵng"
+  },
+  {
+    firstName: "Cường",
+    middleName: "Lê Minh",
+    address: "78 Trần Phú, TP Hải Phòng"
+  },
+  {
+    firstName: "Dương",
+    middleName: "Phạm Hoàng",
+    address: "99 Nguyễn Trãi, Hà Nội"
+  }
+];
